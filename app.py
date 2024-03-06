fd25c7b00a8e7023c28f36a78d14a1cad8eca736
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-03-05 22:22:15 -0700

    started to build models and endpoints

--- app.py
+++ app.py
@@ -10,9 +10,28 @@ from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
+from models import Artist, Piece, Article, DefaultBase
 # from models import User, Task, List, DefaultBase
 
 app.secret_key = 'dkdfkSKDGSDFGdfkvnia;vDAKCni1dkf65kjlfgFJl6kgGsdh4dfgk1fjD3gdKDJ'
+
+class Login(Resource):
+
+    def post(self):
+        req_data = request.get_json()
+        artist = Artist.query.filter(Artist.username == req_data['username']).first()
+
+        try:
+            if artist.auth(req_data['password']) == False:
+                return make_response({'error': 'wrong password enterred'}, 401)
+            
+            session['user_id'] = artist.id
+
+            make_response(artist.to_dict(), 200)
+        
+        except:
+            return make_response({'error':'login error'})
+            
 
 # Views
 
--- models.py
+++ models.py
@@ -5,14 +5,37 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 from config import db, bcrypt
 
-# User
-#
-# Artist
-#
-# Piece
-# 
-# News
-# -title
-# -text
-# Category
-# -pieces
+class DefaultBase(db.Model, SerializerMixin):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Instance of {self.__class__.__name__}, ID: {self.id}>'
+    
+class Artist(DefaultBase):
+    __tablename__ = 'artists'
+
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password = db.Column(db.String, nullable=False)
+
+    name = db.Column(db.String, nullable=False)
+    bio = db.Column(db.Text)
+    _email_address = db.column(db.String)
+
+class Piece(DefaultBase):
+    __tablename__ = 'pieces'
+
+    title = db.Column(db.String, unique=True, nullable=False)
+    price = db.Column(db.Integer)
+
+class Article(DefaultBase):
+    __tablename__ = 'articles'
+
+    title = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+
+
