41dc2926b05472fe93a78be4ca4e25a42cd58e20
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-03-06 23:15:43 -0700

    added classes for messaging system

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
-from models import Artist, Piece, Article, DefaultBase
+from models import Artist, Piece, Article, User, Message, DefaultBase
 # from models import User, Task, List, DefaultBase
 
 app.secret_key = 'dkdfkSKDGSDFGdfkvnia;vDAKCni1dkf65kjlfgFJl6kgGsdh4dfgk1fjD3gdKDJ'
@@ -19,21 +19,71 @@ class Login(Resource):
 
     def post(self):
         req_data = request.get_json()
-        artist = Artist.query.filter(Artist.username == req_data['username']).first()
+        user = User.query.filter(User.username == req_data['username']).first()
 
         try:
-            if artist.auth(req_data['password']) == False:
+            if user.auth(req_data['password']) == False:
                 return make_response({'error': 'wrong password enterred'}, 401)
             
-            session['user_id'] = artist.id
+            session['user_id'] = user.id
 
-            make_response(artist.to_dict(), 200)
+            make_response(user.to_dict(), 200)
         
         except:
             return make_response({'error':'login error'})
-            
+        
+class Logout(Resource):
 
-# Views
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+    
+class CheckSession(Resource):
+
+    def get(self):
+        
+        artist = Artist.query.filter(Artist.id == session.get('user_id')).first()
+
+        if artist:
+            return make_response(artist.to_dict(), 200)
+        else:
+            return make_response({'error':'session not found'}, 401)
+        
+class Artists(Resource):
+    
+    def get(self):
+        artist_list = []
+        for artist in Artist.query.all():
+            artist_list.append(artist.to_dict())
+
+        return make_response(artist_list, 200)
+
+class Pieces(Resource):
+    
+    def get(self):
+        piece_list = []
+        for piece in Piece.query.all():
+            piece_list.append(piece.to_dict())
+
+        return make_response(piece_list, 200)        
+
+class Articles(Resource):
+    
+    def get(self):
+        article_list = []
+        for article in Article.query.all():
+            article_list.append(article.to_dict())
+        
+        return make_response(article_list, 200)            
+
+# Views        
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/checksession')
+api.add_resource(Artist, '/artists')
+api.add_resource(Pieces, '/pieces')
+api.add_resource(Articles, '/articles')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.hybrid import hybrid_property 
 
 from config import db, bcrypt
 
@@ -15,27 +15,77 @@ class DefaultBase(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<Instance of {self.__class__.__name__}, ID: {self.id}>'
     
-class Artist(DefaultBase):
-    __tablename__ = 'artists'
+class User(DefaultBase):
 
     username = db.Column(db.String, unique=True, nullable=False)
     _password = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean)
+    
+    articles = db.relationship('Piece', backref='user', cascade='all, delete-orphan')
+    messages = db.relationship('Message', backref = 'user', cascade='all, delete-orphan')
+    
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+
+    @validates('password')
+    def validate_password (self, key, password):
+        if(4 > len(password) > 35):
+            raise ValueError('Password must be between 5 and 34 characters long.')
+        
+    @validates('username')
+    def validate_username (self, key, username):
+        if(4 > len(username) > 16):
+            raise ValueError('Username must be between 5 and 15 characters long.')
+        
+    @hybrid_property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, password):
+        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self._password = password.hash
+
+    def auth(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+    
+class Artist(DefaultBase):
+    __tablename__ = 'artists'
 
     name = db.Column(db.String, nullable=False)
     bio = db.Column(db.Text)
-    _email_address = db.column(db.String)
+
+    pieces = db.relationship('Piece', backref='artist', cascade='all, delete-orphan')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Piece(DefaultBase):
     __tablename__ = 'pieces'
 
     title = db.Column(db.String, unique=True, nullable=False)
+    img_url = db.Column(db.String)
     price = db.Column(db.Integer)
+
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
 
 class Article(DefaultBase):
     __tablename__ = 'articles'
 
     title = db.Column(db.String, nullable=False)
     start_date = db.Column(db.DateTime)
-    end_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)  
+    archived = db.Column(db.Boolean)
+    img_url = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Message(DefaultBase):
+    __tablename__ = 'messages'
+
+    text = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
 
 
