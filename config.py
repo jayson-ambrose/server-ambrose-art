a338c9d5fda19219a2a204dbc73587e578f00a9b
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-05-30 21:28:10 -0600

    testing

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
 
 app.json.compact = False
 
