f0efb27c9a0f324180ecbd483e07395ede3eb17f
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-05-31 22:12:25 -0600

    config

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_PERMANENT'] = True
 
 app.json.compact = False
 
